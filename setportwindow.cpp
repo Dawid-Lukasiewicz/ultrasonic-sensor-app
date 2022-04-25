8fec4a2f5300d35c32c548d1b0cf6edcab49b5f3
Dawid Łukasiewicz <lukasiewicz@onet.com.pl>
2022-04-25 17:56:35 +0200

    Exported to linux

---
+++ setportwindow.cpp
@@ -0,0 +1,122 @@
+#include "setportwindow.h"
+#include "ui_setportwindow.h"
+
+SetPortWindow::SetPortWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::SetPortWindow)
+{
+    ui->setupUi(this);
+    Device = new QSerialPort;
+}
+
+SetPortWindow::~SetPortWindow()
+{
+    delete ui;
+    delete Device;
+}
+
+void SetPortWindow::SendToDevice(const QString &message)
+{
+    if(Device->isOpen() && Device->isWritable())
+    {
+        SendToLogs(message);
+        Device->write(message.toStdString().c_str());
+    }
+    else
+    {
+        SendToLogs("Failed to send message to device");
+        return;
+    }
+}
+
+
+void SetPortWindow::on_Search_clicked()
+{
+    QList<QSerialPortInfo> devices;
+    devices = QSerialPortInfo::availablePorts();
+    ui->SelectedPort->clear();
+    for(int i = 0; i < devices.count(); i++)
+    {
+        SendToLogs("[FOUND] " + devices.at(i).portName() + "  " + devices.at(i).description());
+        ui->SelectedPort->addItem(devices.at(i).portName() + "  " + devices.at(i).description());
+    }
+
+}
+
+void SetPortWindow::on_Connect_clicked()
+{
+    if(ui->SelectedPort->count() == 0)
+    {
+        SendToLogs("No device found");
+        return;
+    }
+    else if(Device->isOpen())
+    {
+        SendToLogs("Port " + Device->portName() + " already opened");
+        return;
+    }
+
+    QString PortDeviceName = ui->SelectedPort->currentText().split("  ").first();
+    Device->setPortName(PortDeviceName);
+    if(Device->open(QSerialPort::ReadWrite))
+    {
+        Device->setBaudRate(QSerialPort::Baud9600);
+        Device->setParity(QSerialPort::NoParity);
+        Device->setDataBits(QSerialPort::Data8);
+        Device->setStopBits(QSerialPort::OneStop);
+        Device->setFlowControl(QSerialPort::NoFlowControl);
+
+        SendToLogs("Serial port opened");
+
+        connect(this->Device, SIGNAL(readyRead()), this, SLOT(ReadFromPort()));
+    }
+    else
+    {
+        SendToLogs("Failed to open serial port");
+        return;
+    }
+
+}
+
+void SetPortWindow::on_Disconnect_clicked()
+{
+    if(Device->isOpen())
+    {
+        Device->close();
+        SendToLogs("Port disconnected");
+    }
+    else
+    {
+        SendToLogs("No port connected");
+    }
+}
+
+void SetPortWindow::SendToLogs(const QString &message)
+{
+    QString CurrentDate = QDateTime::currentDateTime().toString("yyyy.MM.dd hh:mm:ss -- ");
+    ui->textEditLogs->append(CurrentDate + " " + message);
+}
+
+void SetPortWindow::ReadFromPort()
+{
+    while(Device->canReadLine())
+    {
+        QString line = Device->readLine();
+        int index = line.lastIndexOf("\r");
+        SendToLogs(line.left(index));
+    }
+}
+
+
+
+void SetPortWindow::on_Measurement_clicked()
+{
+    SendToDevice("1");
+}
+
+
+void SetPortWindow::on_Exit_clicked()
+{
+    this->close();
+}
+
---
+++ setportwindow.h
@@ -0,0 +1,39 @@
+#ifndef SETPORTWINDOW_H
+#define SETPORTWINDOW_H
+
+#include <QDebug>
+#include <QList>
+#include <QSerialPortInfo>
+#include <QSerialPort>
+#include <QDateTime>
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class SetPortWindow; }
+QT_END_NAMESPACE
+
+class SetPortWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    SetPortWindow(QWidget *parent = nullptr);
+    ~SetPortWindow();
+    void SendToDevice(const QString &message);
+
+private slots:
+    void on_Search_clicked();
+    void on_Connect_clicked();
+    void on_Disconnect_clicked();
+    void SendToLogs(const QString &message);
+    void ReadFromPort();
+    void on_Measurement_clicked();
+
+    void on_Exit_clicked();
+
+private:
+    Ui::SetPortWindow *ui;
+    QSerialPort *Device;
+};
+#endif // SETPORTWINDOW_H
